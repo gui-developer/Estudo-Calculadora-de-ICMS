1edcaad8cdf67f27e566b1514168c04b0f4ec51d
Guilherme Oliveira <nelson.domiciano.nd@gmail.com>
2025-09-01 23:37:57 -0300

    feat: Adiciona lógica para o cálculo de ICMS Este commit implementa a funcionalidade de cálculo do ICMS no aplicativo.
    Principais alterações:
    UI (activity_main.xml): Adiciona IDs aos campos de texto e ao botão para que possam ser manipulados pelo código.
    -Lógica (MainActivity.java):
    -Obtém os valores da tela.
    -Calcula o valor do ICMS com base no estado e na mercadoria.
    -Exibe o resultado final na interface.

--- app/src/main/java/com/example/faculdadeapp/MainActivity.java
+++ app/src/main/java/com/example/faculdadeapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.faculdadeapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Instanciando os objetos da tela
+    private EditText mEstado;
+    private EditText mValor;
+    private EditText mPorcentagem;
+
+    private EditText mvalorTotal;
+    private Button mCalcular;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //
+
+        mEstado = findViewById(R.id.estado);
+        mValor = findViewById(R.id.valor);
+        mPorcentagem = findViewById(R.id.porcentagem);
+        mvalorTotal = findViewById(R.id.valorTotal);
+        mCalcular = findViewById(R.id.calcular);
+
+    }
+
+    public void calcularICMS(View view) {
+        String estado = mEstado.getText().toString();
+        String valor = mValor.getText().toString();
+
+        float valorFloat = Float.parseFloat(valor);
+
+        float porcentagem = 0;
+
+        switch (estado) {
+            case "SP":
+                porcentagem = 0.45f;
+                break;
+            case "MG":
+                porcentagem = 0.075f;
+                break;
+            case "RO":
+                porcentagem = 17.5f;
+                break;
+        }
+
+        float valorTotal = valorFloat * (1 + porcentagem);
+
+        mvalorTotal.setText(String.format("R$%.2f", valorTotal));
+
     }
 }
